0ad92d1970281f56d36705fd7d45ed8f44abc5de
michael-nhat <nhathoang.haiphong@gmail.com>
2022-12-23 20:49:23 +0700

    init

---
+++ src/pedestal_hello1/pedestal_hello1.clj
@@ -0,0 +1,9 @@
+(ns pedestal-hello1.pedestal-hello1
+  (:require [io.pedestal.http :as http]
+          [io.pedestal.http.route :as route])
+  "FIXME: my new org.corfield.new/scratch project.")
+
+(defn -main
+  "Invoke me with clojure -M -m pedestal-hello1.pedestal-hello1"
+  [& args]
+  (println "-main with" args))
---
+++ test/pedestal_hello1/pedestal_hello1_test.clj
@@ -0,0 +1,6 @@
+(ns pedestal-hello1.pedestal-hello1-test
+  (:require [clojure.test :as t :refer [deftest is testing]]))
+
+(deftest pedestal-hello1-test
+  (testing "TODO: fix"
+    (is (= :foo :bar))))
