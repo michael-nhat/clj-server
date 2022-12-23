ea4e5c5f89f4ffee8bf3414133935f3425a8c36f
michael-nhat <nhathoang.haiphong@gmail.com>
2022-12-23 21:40:52 +0700

    fix

--- src/pedestal_hello1/pedestal_hello1.clj
+++ src/pedestal_hello1/pedestal_hello1.clj
@@ -1,7 +1,15 @@
 (ns pedestal-hello1.pedestal-hello1
-  (:require [io.pedestal.http :as http]
-          [io.pedestal.http.route :as route])
-  "FIXME: my new org.corfield.new/scratch project.")
+  (:require
+   [io.pedestal.http :as http]
+   [io.pedestal.http.route :as route]))
+
+(defn respond-hello [request]
+  {:status 200 :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
 
 (defn -main
   "Invoke me with clojure -M -m pedestal-hello1.pedestal-hello1"
